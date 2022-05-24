5f7391a8053ba12eec057d8846f70ab9d76e855c
Alexis H <75497497+Hexiss@users.noreply.github.com>
2022-05-24 14:54:18 +0200

    rework

--- server.js
+++ server.js
@@ -1,44 +1,13 @@
-'use strict';
-
-//let express = require('express');
-//let bodyParser = require('body-parser');
-//let app = express();
-//let http = require('http').Server(app);
-//let io = require('socket.io')(http);
+let express = require('express');
+let bodyParser = require('body-parser');
+let app = express();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
 let mongoose = require('mongoose');
 
-//app.use(express.static(__dirname));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended: false}))
-
-const fastify = require('fastify')({ logger: true });
-const { connect } = require('mongoose');
-
-//Use cloud
-//let databaseUrl = 'mongodb+srv://yolo:yolo@cluster0.0jc8g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
-//Use local storage
-let databaseUrl = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
-connect(databaseUrl).catch(err =>console.error(err));
-const PORT = 3000;
-
-fastify.register(require('fastify-helmet'));
-fastify.register(require('./routes/myAppR'), { prefix: '/v1/myApp' });
-
-fastify.get('/', async () => { return { msg: "Hello World" }});
-
-// Run the server!
-const start = async () => {
-    try {
-    await fastify.listen(PORT, '0.0.0.0');
-    console.log(fastify.printRoutes());
-    } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
-    }
-   }
-   start();
-
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 
 let Message = mongoose.model('Message',{
   name : String,
@@ -81,3 +50,22 @@ app.post('/messages', async (req, res) => {
 io.on('connection', () =>{
   console.log('a user is connected')
 })
+
+//Use cloud
+//let databaseUrl = 'mongodb+srv://yolo:yolo@cluster0.0jc8g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+//Use local storage
+let databaseUrl = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
+mongoose.connect(databaseUrl, {	
+    useNewUrlParser: true,
+	useUnifiedTopology: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+let server = http.listen(3000, () => {
+ console.log('server is running on port', server.address().port);
+});
